a422b6e467cfe0a159286ef624287d9fd14b6b28
m-rodriguezUC <mrodriguez@unifiedcommunications.com>
2024-08-23 10:40:45 -0600

    List of tasks is displayed

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
+<script setup>
+const taskList = ref([{id: 1, title: "task 1", description: "description 1"}, {id: 2, title: "task 2", description: "description 2"}]);
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Task v-for="task in taskList" :key="task.id" :title="task.title" :description="task.description" />
   </div>
 </template>
---
+++ components/Task.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    title: String,
+    description: String
+});
+</script>
+
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+    </div>
+</template>
